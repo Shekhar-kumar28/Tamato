28d420dc4fd11b561d7f1bcaf94513efe2ea74a4
Shekhar Kumar <sk164949@gmail.com>
2024-05-07 18:13:01 +0530

    Update database connection URL to use environment variable
    Change database connection URL from hardcoded string to using process.env.MONGODB\_URI.
    This allows for easier configuration and better security practices.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://sk164949:Shekhar123@cluster0.utvv8iy.mongodb.net/food-del')
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("Database connected");
     });
 };
-// module.exports = connectDB;
+
 
